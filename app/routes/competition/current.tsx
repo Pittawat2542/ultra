d6d6f3ffdb53e2d67889f52f1b1d6450257d1bb3
Pittawat <11158905+Pittawat2542@users.noreply.github.com>
2022-07-12 18:45:53 +0700

    feat!: current and past competition list pages
    Signed-off-by: Pittawat <11158905+Pittawat2542@users.noreply.github.com>

--- app/routes/competition/current.tsx
+++ app/routes/competition/current.tsx
@@ -1,74 +1,54 @@
 import CompetitionDetailCard from "~/components/Competitions/CompetitionDetailCard/CompetitionDetailCard";
 import Footer from "~/components/Footer/Footer";
-import HeroSection from "~/components/HeroSection/HeroSection";
-import { Link } from "@remix-run/react";
 import NavigationBar from "~/components/NavigationBar/NavigationBar";
-import SectionHeader from "~/components/Competitions/SectionHeader/SectionHeader";
-
-const heroContent = {
-  titleContent: (
-    <h1 className="mb-8 font-serif text-6xl font-bold">Competitions</h1>
-  ),
-  subtitleText:
-    "Explore all current competitions, see stunning posters, take a step further to vote your favorites, and why not submit one yourself!?",
-  heroImageUrl: "/images/hero-competition.png",
-};
 
 export default function CompetitionIndex() {
-  //TODO: Sort data by date descending (newest first) & Only show view all if there is more than 6 competition in each types of competitions
   return (
     <>
       <NavigationBar />
       <main className="px-32">
-        <HeroSection
-          titleContent={heroContent.titleContent}
-          subtitleText={heroContent.subtitleText}
-          heroImageUrl={heroContent.heroImageUrl}
-        />
         <section className="mt-8">
-          <SectionHeader title="Current Competitions" url="current" />
-          <div className="grid grid-cols-3 gap-8">
-            <CompetitionDetailCard
-              title="Deep Learning Artificial Intelligence Summer School 2022 Poster Competition"
-              url=""
-              imageUrl="https://cdn.pixabay.com/photo/2022/06/19/11/07/bird-7271620_1280.jpg"
-              submissionDeadline={new Date("2022-08-01T00:00:00.000Z")}
-            />
-            <CompetitionDetailCard
-              title="Deep Learning Artificial Intelligence Summer School 2022 Poster Competition"
-              url=""
-              imageUrl="https://cdn.pixabay.com/photo/2022/06/19/11/07/bird-7271620_1280.jpg"
-              submissionDeadline={new Date("2022-08-01T00:00:00.000Z")}
-            />
-            <CompetitionDetailCard
-              title="Deep Learning Artificial Intelligence Summer School 2022 Poster Competition"
-              url=""
-              imageUrl="https://cdn.pixabay.com/photo/2022/06/19/11/07/bird-7271620_1280.jpg"
-              submissionDeadline={new Date("2022-08-01T00:00:00.000Z")}
-            />
+          <div className="flex max-w-[50%] flex-col">
+            <h1 className="mb-8 font-serif text-4xl font-bold">
+              Current Competitions
+            </h1>
           </div>
-        </section>
-        <hr className="my-16 opacity-50" />
-        <section>
-          <SectionHeader title="Past Competitions" url="past" />
           <div className="grid grid-cols-3 gap-8">
             <CompetitionDetailCard
               title="Deep Learning Artificial Intelligence Summer School 2022 Poster Competition"
               url=""
-              imageUrl="https://cdn.pixabay.com/photo/2022/07/04/17/16/dove-7301617_1280.jpg"
-              submissionDeadline={new Date("2020-08-01T00:00:00.000Z")}
+              imageUrl="https://cdn.pixabay.com/photo/2022/06/19/11/07/bird-7271620_1280.jpg"
+              submissionDeadline={new Date("2022-08-01T00:00:00.000Z")}
             />
             <CompetitionDetailCard
               title="Deep Learning Artificial Intelligence Summer School 2022 Poster Competition"
               url=""
-              imageUrl="https://cdn.pixabay.com/photo/2022/07/04/17/16/dove-7301617_1280.jpg"
-              submissionDeadline={new Date("2020-08-01T00:00:00.000Z")}
+              imageUrl="https://cdn.pixabay.com/photo/2022/06/19/11/07/bird-7271620_1280.jpg"
+              submissionDeadline={new Date("2022-08-01T00:00:00.000Z")}
             />
             <CompetitionDetailCard
               title="Deep Learning Artificial Intelligence Summer School 2022 Poster Competition"
               url=""
-              imageUrl="https://cdn.pixabay.com/photo/2022/07/04/17/16/dove-7301617_1280.jpg"
-              submissionDeadline={new Date("2020-08-01T00:00:00.000Z")}
+              imageUrl="https://cdn.pixabay.com/photo/2022/06/19/11/07/bird-7271620_1280.jpg"
+              submissionDeadline={new Date("2022-08-01T00:00:00.000Z")}
+            />
+            <CompetitionDetailCard
+              title="Deep Learning Artificial Intelligence Summer School 2022 Poster Competition"
+              url=""
+              imageUrl="https://cdn.pixabay.com/photo/2022/06/19/11/07/bird-7271620_1280.jpg"
+              submissionDeadline={new Date("2022-08-01T00:00:00.000Z")}
+            />
+            <CompetitionDetailCard
+              title="Deep Learning Artificial Intelligence Summer School 2022 Poster Competition"
+              url=""
+              imageUrl="https://cdn.pixabay.com/photo/2022/06/19/11/07/bird-7271620_1280.jpg"
+              submissionDeadline={new Date("2022-08-01T00:00:00.000Z")}
+            />
+            <CompetitionDetailCard
+              title="Deep Learning Artificial Intelligence Summer School 2022 Poster Competition"
+              url=""
+              imageUrl="https://cdn.pixabay.com/photo/2022/06/19/11/07/bird-7271620_1280.jpg"
+              submissionDeadline={new Date("2022-08-01T00:00:00.000Z")}
             />
           </div>
         </section>
