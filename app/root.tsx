a7b659e24190bcdb7b6b57e62071ab16610a8d8d
Pittawat <11158905+Pittawat2542@users.noreply.github.com>
2022-07-19 12:25:51 +0700

    feat: prepare authentication logic

--- app/root.tsx
+++ app/root.tsx
@@ -6,9 +6,11 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
-import type { LinksFunction, MetaFunction } from "@remix-run/node";
+import type { LinksFunction, LoaderArgs, MetaFunction } from "@remix-run/node";
 
 import customStylesheetUrl from "./styles/custom.css";
+import { getUser } from "./session.server";
+import { json } from "@remix-run/node";
 import tailwindStylesheetUrl from "./styles/tailwind.css";
 
 export const links: LinksFunction = () => {
@@ -36,6 +38,12 @@ export const meta: MetaFunction = () => ({
   title: "Ultra - Experience AR Posters Like Never Before",
   viewport: "width=device-width,initial-scale=1",
 });
+
+export async function loader({ request }: LoaderArgs) {
+  return json({
+    user: await getUser(request),
+  });
+}
 
 export default function App() {
   return (
--- app/utils.ts
+++ app/utils.ts
@@ -1,3 +1,4 @@
+import type { User } from "~/models/user.server";
 import { useMatches } from "@remix-run/react";
 import { useMemo } from "react";
 
@@ -36,8 +37,34 @@ export function useMatchesData(
 ): Record<string, unknown> | undefined {
   const matchingRoutes = useMatches();
   const route = useMemo(
-    () => matchingRoutes.find((route) => route.id === id),
+    () => matchingRoutes.find((r) => r.id === id),
     [matchingRoutes, id]
   );
   return route?.data;
+}
+
+function isUser(user: any): user is User {
+  return user && typeof user === "object" && typeof user.email === "string";
+}
+
+export function useOptionalUser(): User | undefined {
+  const data = useMatchesData("root");
+  if (!data || !isUser(data.user)) {
+    return undefined;
+  }
+  return data.user;
+}
+
+export function useUser(): User {
+  const maybeUser = useOptionalUser();
+  if (!maybeUser) {
+    throw new Error(
+      "No user found in root loader, but user is required by useUser. If user is optional, try useOptionalUser instead."
+    );
+  }
+  return maybeUser;
+}
+
+export function validateEmail(email: unknown): email is string {
+  return typeof email === "string" && email.length > 3 && email.includes("@");
 }
