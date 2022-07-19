318b5afcf0c4689fd6f16177fe0be5d5f793e236
Pittawat <11158905+Pittawat2542@users.noreply.github.com>
2022-07-19 12:29:24 +0700

    feat: fetch data from db for competition detail

--- app/routes/competition/c/$competitionSlug/index.tsx
+++ app/routes/competition/c/$competitionSlug/index.tsx
@@ -2,6 +2,8 @@ import {
   ChartBarIcon,
   ClockIcon,
   GlobeIcon,
+  LockClosedIcon,
+  QuestionMarkCircleIcon,
   SearchIcon,
   StarIcon,
   UserGroupIcon,
@@ -10,26 +12,68 @@ import {
 import Button from "~/components/Button/Button";
 import Divider from "~/components/Divider/Divider";
 import Footer from "~/components/Footer/Footer";
-import { Link } from "@remix-run/react";
+import { Link, useLoaderData } from "@remix-run/react";
 import NavigationBar from "~/components/NavigationBar/NavigationBar";
 import PageHeader from "~/components/Competitions/PageHeader/PageHeader";
 import PosterCard from "~/components/Competitions/PosterCard/PosterCard";
 import { formatDateTimeString } from "~/utils/time";
+import { json } from "@remix-run/node";
+import type { LoaderFunction } from "@remix-run/node";
+import { prisma } from "~/db.server";
+import type { Competition, Poster } from "@prisma/client";
+import { DEFAULT_COVER_IMAGE } from "~/constants/images";
+import EmptyState from "~/components/EmptyState/EmptyState";
+
+type LoaderData = {
+  competition: Competition;
+  posters: Poster[];
+};
+
+export const loader: LoaderFunction = async ({ params }) => {
+  const { competitionSlug } = params;
+  const competition = await prisma.competition.findUnique({
+    where: {
+      slug: competitionSlug,
+    },
+  });
+
+  if (!competition) {
+    throw new Response("Not Found", {
+      status: 404,
+    });
+  }
+
+  const posters = await prisma.poster.findMany({
+    where: {
+      competitionId: competition.id,
+    },
+  });
+
+  return json<LoaderData>({ competition, posters });
+};
 
 export default function CompetitionDetailIndex() {
-  //TODO: Handle empty state when there is no poster submitted & more configuration display and ranking mode after rank announcement date passed
+  const { competition, posters } = useLoaderData<LoaderData>();
+  let hasRankAnnounced = false;
+
+  if (competition.votingPrivacy !== "DISABLED") {
+    hasRankAnnounced = competition.rankAnnouncementDate! >= new Date();
+  }
+
+  //TODO: Define rank calculation logic and supply to the required components
+
   return (
     <>
       <NavigationBar />
       <main className="mt-8 px-32">
         <section>
           <div className="flex items-center justify-between gap-16">
-            <PageHeader title="Deep Learning Artificial Intelligence Summer School 2022 Poster Competition" />
+            <PageHeader title={competition.title} />
             <div className="flex flex-1 flex-col items-center">
               <img
                 className="rounded-xl object-contain transition group-hover:opacity-70"
-                src="https://cdn.pixabay.com/photo/2022/06/19/11/07/bird-7271620_1280.jpg"
-                alt=""
+                src={competition.coverImagePath ?? DEFAULT_COVER_IMAGE}
+                alt={`${competition.title} cover`}
               />
               <div className="mt-6 flex w-full justify-between gap-4">
                 <Link className="block w-full" to="ar">
@@ -44,106 +88,132 @@ export default function CompetitionDetailIndex() {
         </section>
         <section className="mt-8">
           <h1 className="mb-4 font-serif text-3xl font-bold">Schedule</h1>
-          <p className="mb-3 text-xl">
-            <ClockIcon className="mr-1 -mt-1 inline-block h-6 w-6" />{" "}
-            <span className="mr-4 font-bold">Submission</span>
-            {`${formatDateTimeString(
-              new Date("2020-08-01T00:00:00.000Z")
-            )} - ${formatDateTimeString(new Date("2020-08-30T00:00:00.000Z"))}`}
-          </p>
-          <p className="mb-3 text-xl">
-            <StarIcon className="mr-1 -mt-1 inline-block h-6 w-6" />{" "}
-            <span className="mr-4 font-bold">Voting</span>
-            {`${formatDateTimeString(
-              new Date("2020-09-01T00:00:00.000Z")
-            )} - ${formatDateTimeString(new Date("2020-09-14T00:00:00.000Z"))}`}
-          </p>
-          <p className="mb-3 text-xl">
-            <ChartBarIcon className="mr-1 -mt-1 inline-block h-6 w-6" />{" "}
-            <span className="mr-4 font-bold">Rank Announcement</span>
-            {formatDateTimeString(new Date("2020-09-15T00:00:00.000Z"))}
-          </p>
+          {competition.submissionPrivacy !== "DISABLED" && (
+            <p className="mb-3 text-xl">
+              <ClockIcon className="mr-1 -mt-1 inline-block h-6 w-6" />{" "}
+              <span className="mr-4 font-bold">Submission</span>
+              {`${formatDateTimeString(
+                competition.submissionStart!
+              )} - ${formatDateTimeString(competition.submissionEnd!)}`}
+            </p>
+          )}
+          {competition.votingPrivacy !== "DISABLED" && (
+            <>
+              <p className="mb-3 text-xl">
+                <StarIcon className="mr-1 -mt-1 inline-block h-6 w-6" />{" "}
+                <span className="mr-4 font-bold">Voting</span>
+                {`${formatDateTimeString(
+                  competition.votingStart!
+                )} - ${formatDateTimeString(competition.votingEnd!)}`}
+              </p>
+              <p className="mb-3 text-xl">
+                <ChartBarIcon className="mr-1 -mt-1 inline-block h-6 w-6" />{" "}
+                <span className="mr-4 font-bold">Rank Announcement</span>
+                {formatDateTimeString(competition.rankAnnouncementDate!)}
+              </p>
+            </>
+          )}
         </section>
         <Divider className="mt-6 mb-4" />
         <section className="flex justify-between gap-4">
-          <p className="mb-3 text-xl">
-            <GlobeIcon className="mr-2 -mt-1 inline-block h-6 w-6" />{" "}
-            <span className="mr-4 font-bold">Anyone Can View</span>
-          </p>
-          <p className="mb-3 text-xl">
-            <GlobeIcon className="mr-2 -mt-1 inline-block h-6 w-6" />{" "}
-            <span className="mr-4 font-bold">Anyone Can Submit</span>
-          </p>
-          <p className="mb-3 text-xl">
-            <UserGroupIcon className="mr-2 -mt-1 inline-block h-6 w-6" />{" "}
-            <span className="mr-4 font-bold">Allow Team Submission</span>
-          </p>
-          <p className="mb-3 text-xl">
-            <SearchIcon className="mr-2 -mt-1 inline-block h-6 w-6" />{" "}
-            <span className="mr-4 font-bold">Submission Require Approval</span>
-          </p>
+          {competition.viewingPrivacy === "PUBLIC" && (
+            <p className="mb-3 text-xl">
+              <GlobeIcon className="mr-2 -mt-1 inline-block h-6 w-6" />{" "}
+              <span className="mr-4 font-bold">Anyone Can View</span>
+            </p>
+          )}
+          {competition.viewingPrivacy === "LIMITED" && (
+            <p className="mb-3 text-xl">
+              <LockClosedIcon className="mr-2 -mt-1 inline-block h-6 w-6" />{" "}
+              <span className="mr-4 font-bold">Limited Visibility</span>
+            </p>
+          )}
+
+          {competition.submissionPrivacy === "PUBLIC" && (
+            <p className="mb-3 text-xl">
+              <GlobeIcon className="mr-2 -mt-1 inline-block h-6 w-6" />{" "}
+              <span className="mr-4 font-bold">Anyone Can Submit</span>
+            </p>
+          )}
+          {competition.submissionPrivacy === "LIMITED" && (
+            <p className="mb-3 text-xl">
+              <LockClosedIcon className="mr-2 -mt-1 inline-block h-6 w-6" />{" "}
+              <span className="mr-4 font-bold">Limited Submission</span>
+            </p>
+          )}
+
+          {competition.votingPrivacy === "PUBLIC" && (
+            <p className="mb-3 text-xl">
+              <GlobeIcon className="mr-2 -mt-1 inline-block h-6 w-6" />{" "}
+              <span className="mr-4 font-bold">Anyone Can Vote</span>
+            </p>
+          )}
+          {competition.votingPrivacy === "LIMITED" && (
+            <p className="mb-3 text-xl">
+              <LockClosedIcon className="mr-2 -mt-1 inline-block h-6 w-6" />{" "}
+              <span className="mr-4 font-bold">Limited Voting</span>
+            </p>
+          )}
+
+          {competition.teamSubmission === "ENABLED" && (
+            <p className="mb-3 text-xl">
+              <UserGroupIcon className="mr-2 -mt-1 inline-block h-6 w-6" />{" "}
+              <span className="mr-4 font-bold">Allow Team Submission</span>
+            </p>
+          )}
+          {competition.teamSubmission === "ENABLED" && competition.maxTeamSize && (
+            <p className="mb-3 text-xl">
+              <UserGroupIcon className="mr-2 -mt-1 inline-block h-6 w-6" />{" "}
+              <span className="mr-4 font-bold">
+                1 - {competition.maxTeamSize} Team Members
+              </span>
+            </p>
+          )}
+
+          {competition.submissionPolicy === "REQUIRE_APPROVAL" && (
+            <p className="mb-3 text-xl">
+              <SearchIcon className="mr-2 -mt-1 inline-block h-6 w-6" />{" "}
+              <span className="mr-4 font-bold">
+                Submission Require Approval
+              </span>
+            </p>
+          )}
         </section>
         <Divider className="mt-2 mb-16" />
         <section className="text-lg">
-          <p className="mb-2">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lacus sit
-            est maecenas curae consectetur viverra ac faucibus lectus mus
-            dapibus odio. Eget quam sapien suscipit facilisi tempor phasellus
-            tortor cum aptent neque quam orci. Convallis nec litora conubia
-            faucibus taciti fringilla conubia malesuada ridiculus platea
-            elementum erat.
-          </p>
-          <p className="mb-2">
-            Etiam class convallis viverra inceptos sodales ridiculus per lacinia
-            aenean at curabitur praesent. Elementum mauris tempus semper tellus
-            vivamus erat pellentesque sociosqu iaculis eleifend quis fermentum.
-            Curabitur vestibulum lobortis habitasse diam eros mattis porta
-            vulputate erat eu euismod semper. Scelerisque lacinia vestibulum
-            quam vulputate imperdiet vivamus suspendisse enim potenti convallis
-            etiam massa.
-          </p>
-          <p className="mb-2">
-            Et vel ornare purus sem volutpat natoque ad sollicitudin dictum
-            risus interdum nascetur. Morbi nullam lorem erat nisi taciti primis
-            hendrerit congue cubilia aptent rutrum tellus. Quis erat placerat
-            ultrices lacus molestie nascetur ac habitasse integer sociis at
-            ultricies. Et tellus amet consequat curae montes feugiat primis sem
-            ipsum ultrices sit class.
-          </p>
-          <h3 className="mt-6 mb-2 text-lg font-bold">Rules</h3>
-          <p className="mb-2">
-            Et vel ornare purus sem volutpat natoque ad sollicitudin dictum
-            risus interdum nascetur. Morbi nullam lorem erat nisi taciti primis
-            hendrerit congue cubilia aptent rutrum tellus. Quis erat placerat
-            ultrices lacus molestie nascetur ac habitasse integer sociis at
-            ultricies. Et tellus amet consequat curae montes feugiat primis sem
-            ipsum ultrices sit class.
-          </p>
+          {competition.description.split("\n").map((text, index) => (
+            <p key={index} className="mb-2">
+              {text}
+            </p>
+          ))}
         </section>
         <section className="mt-16">
           <h2 className="mb-8 font-serif text-4xl font-bold leading-10">
-            Posters
+            {hasRankAnnounced ? "Ranking" : "Posters"}
           </h2>
-          <div className="grid grid-cols-3 gap-8">
-            <PosterCard
-              title="F.E.A.S.T: Fully-elastic Architecture Strategy for Training Neural Network"
-              url="p/test"
-              imageUrl="https://cdn.pixabay.com/photo/2022/07/08/02/44/pet-7308330_1280.jpg"
-              authors={["John Doe", "Jane Doe"]}
+          {posters.length === 0 ? (
+            <EmptyState
+              iconContent={<QuestionMarkCircleIcon className="h-20 w-20" />}
+              text="There is no poster submitted to this competition."
             />
-            <PosterCard
-              title="Decepticon: An Alternative to Transformers Architecture"
-              url="p/test"
-              imageUrl="https://cdn.pixabay.com/photo/2022/07/06/18/34/mountains-7305769_1280.jpg"
-              authors={["John Doe", "Jane Doe", "Kane Doe"]}
-            />
-            <PosterCard
-              title="F.E.A.S.T: Fully-elastic Architecture Strategy for Training Neural Network"
-              url="p/test"
-              imageUrl="https://cdn.pixabay.com/photo/2022/07/08/02/44/pet-7308330_1280.jpg"
-              authors={["John Doe", "Jane Doe", "Kane Doe", "Peter Doe"]}
-            />
-          </div>
+          ) : (
+            <div
+              className={`grid ${
+                hasRankAnnounced ? "grid-cols-1" : "grid-cols-3"
+              } gap-8`}
+            >
+              {posters.map((poster) => (
+                <PosterCard
+                  key={poster.id}
+                  title={poster.title}
+                  url={`p/${poster.slug}`}
+                  imageUrl={poster.posterImagePath}
+                  authors={poster.authors}
+                  rank={hasRankAnnounced ? 1 : undefined}
+                />
+              ))}
+            </div>
+          )}
         </section>
       </main>
       <Footer />
