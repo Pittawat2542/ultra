619649d9ed8ef27d179e53ec22ca3238fd074ec4
Pittawat <11158905+Pittawat2542@users.noreply.github.com>
2022-07-19 19:38:05 +0700

    feat: fetch data from db for competition listing

--- app/routes/competition/current.tsx
+++ app/routes/competition/current.tsx
@@ -1,55 +1,66 @@
+import type { Competition } from "@prisma/client";
 import CompetitionCard from "~/components/Competitions/CompetitionCard/CompetitionCard";
+import EmptyState from "~/components/EmptyState/EmptyState";
 import Footer from "~/components/Footer/Footer";
+import type { LoaderFunction } from "@remix-run/node";
 import NavigationBar from "~/components/NavigationBar/NavigationBar";
 import PageHeader from "~/components/Competitions/PageHeader/PageHeader";
+import { QuestionMarkCircleIcon } from "@heroicons/react/outline";
+import { json } from "@remix-run/node";
+import { prisma } from "~/db.server";
+import { useLoaderData } from "@remix-run/react";
+
+type LoaderData = {
+  currentCompetitions: Array<Competition>;
+};
+
+export const loader: LoaderFunction = async () => {
+  const currentCompetitions = await prisma.competition.findMany({
+    where: {
+      viewingPrivacy: "PUBLIC",
+      submissionPrivacy: "PUBLIC",
+      submissionEnd: {
+        gte: new Date(),
+      },
+    },
+    take: 6,
+    orderBy: {
+      submissionEnd: "desc",
+    },
+  });
+
+  return json<LoaderData>({ currentCompetitions });
+};
 
 export default function CurrentCompetitions() {
+  const { currentCompetitions } = useLoaderData<LoaderData>();
+
   return (
     <>
       <NavigationBar />
       <main className="px-32">
         <section className="mt-8">
-          <div className="flex max-w-[50%] flex-col">
+          <div className="flex max-w-[50%] flex-col mb-16">
             <PageHeader title="Current Competition" />
           </div>
-          <div className="grid grid-cols-3 gap-8">
-            <CompetitionCard
-              title="Deep Learning Artificial Intelligence Summer School 2022 Poster Competition"
-              url=""
-              imageUrl="https://cdn.pixabay.com/photo/2022/06/19/11/07/bird-7271620_1280.jpg"
-              submissionDeadline={new Date("2022-08-01T00:00:00.000Z")}
+          {currentCompetitions.length === 0 ? (
+            <EmptyState
+              iconContent={<QuestionMarkCircleIcon className="h-20 w-20" />}
+              text="There is no current competitions currently available."
             />
-            <CompetitionCard
-              title="Deep Learning Artificial Intelligence Summer School 2022 Poster Competition"
-              url=""
-              imageUrl="https://cdn.pixabay.com/photo/2022/06/19/11/07/bird-7271620_1280.jpg"
-              submissionDeadline={new Date("2022-08-01T00:00:00.000Z")}
-            />
-            <CompetitionCard
-              title="Deep Learning Artificial Intelligence Summer School 2022 Poster Competition"
-              url=""
-              imageUrl="https://cdn.pixabay.com/photo/2022/06/19/11/07/bird-7271620_1280.jpg"
-              submissionDeadline={new Date("2022-08-01T00:00:00.000Z")}
-            />
-            <CompetitionCard
-              title="Deep Learning Artificial Intelligence Summer School 2022 Poster Competition"
-              url=""
-              imageUrl="https://cdn.pixabay.com/photo/2022/06/19/11/07/bird-7271620_1280.jpg"
-              submissionDeadline={new Date("2022-08-01T00:00:00.000Z")}
-            />
-            <CompetitionCard
-              title="Deep Learning Artificial Intelligence Summer School 2022 Poster Competition"
-              url=""
-              imageUrl="https://cdn.pixabay.com/photo/2022/06/19/11/07/bird-7271620_1280.jpg"
-              submissionDeadline={new Date("2022-08-01T00:00:00.000Z")}
-            />
-            <CompetitionCard
-              title="Deep Learning Artificial Intelligence Summer School 2022 Poster Competition"
-              url=""
-              imageUrl="https://cdn.pixabay.com/photo/2022/06/19/11/07/bird-7271620_1280.jpg"
-              submissionDeadline={new Date("2022-08-01T00:00:00.000Z")}
-            />
-          </div>
+          ) : (
+            <div className="grid grid-cols-3 gap-8">
+              {currentCompetitions.map((competition) => (
+                <CompetitionCard
+                  key={competition.id}
+                  title={competition.title}
+                  url={`c/${competition.slug}`}
+                  imageUrl={competition.coverImagePath}
+                  submissionDeadline={competition.submissionEnd!}
+                />
+              ))}
+            </div>
+          )}
         </section>
       </main>
       <Footer />
