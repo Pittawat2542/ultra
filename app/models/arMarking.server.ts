7d5ab8df426ede29cbadffad1cb10835f4575185
Pittawat <11158905+Pittawat2542@users.noreply.github.com>
2022-07-28 13:45:14 +0700

    feat: remove associated files when delete marking

--- app/models/arMarking.server.ts
+++ app/models/arMarking.server.ts
@@ -1,5 +1,7 @@
 import type { ARMarking, Poster } from "@prisma/client";
 
+import fs from 'fs/promises'
+import path from "path";
 import { prisma } from "~/db.server";
 
 export async function hasARMarkingExisted(posterId: Poster["id"]) {
@@ -19,9 +21,28 @@ export async function createNewARMarking(data: Omit<ARMarking, "id" | "createdAt
 }
 
 export async function deleteARMarking(arMarkingId: ARMarking["id"]) {
-  return prisma.aRMarking.delete({
+  const deletedMarking = await prisma.aRMarking.delete({
     where: {
       id: arMarkingId
+    }
+  });
+  const filePath = path.resolve(deletedMarking.markingImagePath.substring(1));
+  await fs.unlink(filePath)
+  if (deletedMarking.associatedMediaType !== 'TEXT') {
+    const fPath = path.resolve(deletedMarking.associatedMediaPath.substring(1));
+    await fs.unlink(fPath)
+  }
+
+  return deletedMarking;
+}
+
+export async function getAllMarkingTargetImagePaths(posterId: Poster["id"]) {
+  return prisma.aRMarking.findMany({
+    where: {
+      posterId
+    },
+    select: {
+      markingImagePath: true
     }
   });
 }
